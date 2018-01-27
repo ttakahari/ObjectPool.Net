943268e602bf841a89b9b5a5ea09afe7c59b8eb6
t.takahari <taiga1018@gmail.com>
2018-01-27 15:27:08 +0900

    improve FixedSizeObjectPoolClient.

--- src/ObjectPool.Net/FixedSizeObjectPoolClient.cs
+++ src/ObjectPool.Net/FixedSizeObjectPoolClient.cs
@@ -8,45 +8,52 @@ namespace ObjectPool.Net
     {
         private readonly ConcurrentQueue<T> _queue;
         private readonly int _capacity;
+        private readonly Func<T> _valueFactory;
 
         public int DisposedCount { get; private set; }
 
+        public int PoolCount { get; private set; }
+
         public FixedSizeObjectPoolClient(int capacity, Func<T> valueFactory)
         {
-            if (capacity < 1)         throw new ArgumentOutOfRangeException(nameof(capacity), $"The argument '{nameof(capacity)}' must be greater than zero.");
-            if (valueFactory == null) throw new ArgumentNullException(nameof(valueFactory));
+            if (capacity < 1)
+            {
+                throw new ArgumentOutOfRangeException(nameof(capacity), $"The argument '{nameof(capacity)}' must be greater than zero.");
+            }
 
-            _queue    = new ConcurrentQueue<T>(Enumerable.Range(0, capacity).Select(_ => valueFactory.Invoke()));
-            _capacity = capacity;
+            _queue        = new ConcurrentQueue<T>();
+            _capacity     = capacity;
+            _valueFactory = valueFactory ?? throw new ArgumentNullException(nameof(valueFactory));
         }
 
         public T Get()
         {
-            while (true)
+            T value;
+
+            while (!_queue.TryDequeue(out value))
             {
-                if (_queue.TryDequeue(out var value))
+                if (PoolCount < _capacity)
                 {
-                    return value;
+                    value = _valueFactory.Invoke();
+
+                    PoolCount++;
+
+                    break;
                 }
             }
+
+            return value;
         }
 
         public void Set(T value)
         {
-            lock (_queue)
+            if (PoolCount < _capacity)
             {
-                if (_queue.Count < _capacity)
-                {
-                    _queue.Enqueue(value);
-                }
-                else
-                {
-                    var generation = GC.GetGeneration(value);
-
-                    GC.Collect(generation);
-
-                    DisposedCount++;
-                }
+                _queue.Enqueue(value);
+            }
+            else
+            {
+                DisposedCount++;
             }
         }
     }
